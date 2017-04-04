86e6dda992fc413a4993ed0a49072d1839f694b4
Carlos Ivan Hernandez <carlos.i.hernandez011@gmail.com>
2017-04-04 11:32:45 -0400

    Add function to add user to database in FBManager

--- Chef/Chef/FirebaseManager.swift
+++ Chef/Chef/FirebaseManager.swift
@@ -4,6 +4,8 @@ import FirebaseDatabase
 
 class FirebaseManager {
 
+    static let usersRef = FIRDatabase.database().reference().child("users")
+
     static func login(email: String, password: String, completion: ((Bool) -> ())?) {
         FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
             if let error = error {
@@ -23,9 +25,15 @@ class FirebaseManager {
                 completion?(false)
             } else if let user = user {
                 print("\nUser \(user.description) successfully logged in\n")
+                addUser()
                 completion?(true)
             }
         })
+    }
+
+    private static func addUser() {
+        let defaults = ["favRecipes": ["0": "default"], "fridge": ["0": "default"]]
+        usersRef.childByAutoId().setValue(defaults)
     }
 
 }
