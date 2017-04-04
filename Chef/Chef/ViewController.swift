fce803c8a647578639c322f8811a605042991eb6
Carlos Ivan Hernandez <carlos.i.hernandez011@gmail.com>
2017-04-04 11:33:13 -0400

    Add login capabilities to initial VC & refactor

--- Chef/Chef/ViewController.swift
+++ Chef/Chef/ViewController.swift
@@ -1,88 +1,106 @@
-//
-//  ViewController.swift
-//  Chef
-//
-//  Created by Carlos Hernandez on 4/3/17.
-//  Copyright © 2017 Blue Team. All rights reserved.
-//
-
 import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
     
-        var usernameTextField = UITextField()
-        var passwordTextField = UITextField()
-        var loginButton = UIButton()
-    
-    
-    
+    var usernameTextField: UITextField!
+    var passwordTextField: UITextField!
+    var loginButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.view.backgroundColor = .white
-        createBackgroundImage()
-        createUsernameTextField()
-        createPasswordField()
-        createButton()
+        self.createUI()
     }
-    
-    
+
+    func signUpButtonTapped() {
+        guard let email = self.usernameTextField.text, let password = self.passwordTextField.text else {
+            return
+        }
+        FirebaseManager.signUp(email: email, password: password, completion: nil)
+    }
+
+}
+
+
+extension ViewController {
+
+    func createUI() {
+        self.createBackgroundImage()
+        self.createUsernameTextField()
+        self.createPasswordField()
+        self.createButton()
+    }
+
     func createUsernameTextField() {
+        self.usernameTextField = {
+            let tf = UITextField()
+            tf.backgroundColor = .white
+            tf.backgroundColor = .white
+            tf.layer.shadowColor = UIColor.lightGray.cgColor
+            tf.layer.shadowOpacity = 2
+            tf.layer.shadowRadius = 5
+            tf.layer.shadowOffset = CGSize.zero
+            tf.autocapitalizationType = .none
+            tf.autocorrectionType = .no
+            return tf
+        }()
+
         self.view.addSubview(self.usernameTextField)
-        usernameTextField.backgroundColor = .white
-        self.usernameTextField.layer.shadowColor = UIColor.lightGray.cgColor
-        self.usernameTextField.layer.shadowOpacity = 2
-        self.usernameTextField.layer.shadowRadius = 5
-        self.usernameTextField.layer.shadowOffset = CGSize.zero
-        
+
         self.usernameTextField.snp.makeConstraints { (make) in
             make.width.equalToSuperview().multipliedBy(0.8)
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview().multipliedBy(0.66)
         }
     }
-    
+
     func createPasswordField() {
+        self.passwordTextField = {
+            let tf = UITextField()
+            tf.backgroundColor = .white
+            tf.autocapitalizationType = .none
+            tf.isSecureTextEntry = true
+            return tf
+        }()
+
         self.view.addSubview(self.passwordTextField)
-        passwordTextField.backgroundColor = .white
-        
+
         self.passwordTextField.snp.makeConstraints { (make) in
             make.width.equalToSuperview().multipliedBy(0.8)
             make.centerX.equalToSuperview()
             make.top.equalTo(self.usernameTextField.snp.bottom).offset(20)
         }
     }
-    
+
     func createBackgroundImage() {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "food-background")
+
         self.view.addSubview(imageView)
+
         imageView.snp.makeConstraints { (make) in
             make.top.left.height.width.equalToSuperview()
         }
-        self.view.backgroundColor = .white
     }
-    
+
     func createButton() {
-        
-        loginButton.backgroundColor = .white
-        loginButton.setTitle("Login", for: .normal)
-        loginButton.setTitleColor(.black, for: .normal)
-        
+        self.loginButton = {
+            let lb = UIButton()
+            lb.backgroundColor = .white
+            lb.setTitle("Login", for: .normal)
+            lb.setTitleColor(.black, for: .normal)
+            return lb
+        }()
+
         self.view.addSubview(self.loginButton)
-        loginButton.snp.makeConstraints { (make) in
+
+        self.loginButton.snp.makeConstraints { (make) in
             make.width.equalToSuperview().multipliedBy(0.3)
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview()
         }
-        
-        loginButton.addTarget(self, action: #selector(loginButtonPressed), for: .touchUpInside)
-    }
 
-    func loginButtonPressed() {
-        
+        self.loginButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
     }
-
 
 }
-
