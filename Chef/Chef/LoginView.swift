6cdece9b1547a053e007f44e7868a2e0f3746a24
Carlos Ivan Hernandez <carlos.i.hernandez011@gmail.com>
2017-04-10 11:25:36 -0400

    Abstract out LoginView & add UIView extension

---
+++ Chef/Chef/LoginView.swift
@@ -0,0 +1,120 @@
+import UIKit
+
+class LoginView: UIView {
+
+    var usernameTextField: LoginTextField!
+    var passwordTextField: LoginTextField!
+    var loginSignupButton: UIButton!
+    var switchButton: UIButton!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.commonInit()
+    }
+
+    func commonInit() {
+        self.createBackgroundImage()
+        self.createHeaderText()
+        self.createUsernameTextField()
+        self.createPasswordField()
+        self.createLoginSignupButton()
+        self.createSwitchButton()
+    }
+
+    func createBackgroundImage() {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "food-background")
+
+        self.addSubview(imageView)
+
+        imageView.snp.makeConstraints { (make) in
+            make.top.left.height.width.equalToSuperview()
+        }
+    }
+
+    func createHeaderText() {
+        let headerLabel = UILabel()
+        headerLabel.text = "Chef"
+        headerLabel.textColor = .white
+        headerLabel.font = UIFont(name: "Avenir-Medium", size: 48)
+
+        self.addSubview(headerLabel)
+
+        headerLabel.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(75)
+        }
+    }
+
+    func createUsernameTextField() {
+        self.usernameTextField = LoginTextField()
+        self.usernameTextField.keyboardType = .emailAddress
+
+        self.addSubview(self.usernameTextField)
+
+        self.usernameTextField.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().multipliedBy(0.66)
+            make.width.equalToSuperview().multipliedBy(0.8)
+            make.height.equalTo(28)
+        }
+    }
+
+    func createPasswordField() {
+        self.passwordTextField = LoginTextField()
+        self.passwordTextField.isSecureTextEntry = true
+
+        self.addSubview(self.passwordTextField)
+
+        self.passwordTextField.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(self.usernameTextField.snp.bottom).offset(20)
+            make.width.equalToSuperview().multipliedBy(0.8)
+            make.height.equalTo(28)
+        }
+    }
+
+    func createLoginSignupButton() {
+        self.loginSignupButton = {
+            let lb = UIButton()
+            lb.backgroundColor = .white
+            lb.setTitle("Login", for: .normal)
+            lb.setTitleColor(.black, for: .normal)
+            lb.titleLabel?.font = UIFont(name: "Avenir-Heavy", size: 18)
+            lb.layer.cornerRadius = 5
+            return lb
+        }()
+
+        self.addSubview(self.loginSignupButton)
+
+        self.loginSignupButton.snp.makeConstraints { (make) in
+            make.width.equalToSuperview().multipliedBy(0.3)
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+    }
+
+    func createSwitchButton() {
+        self.switchButton = {
+            let sb = UIButton()
+            sb.backgroundColor = .clear
+            sb.setTitle("Sign Up", for: .normal)
+            sb.titleLabel?.textColor = .white
+            sb.titleLabel?.font = UIFont(name: "Avenir-Heavy", size: 16)
+            return sb
+        }()
+
+        self.addSubview(self.switchButton)
+
+        self.switchButton.snp.makeConstraints { (make) in
+            make.right.equalToSuperview().offset(-16)
+            make.bottom.equalToSuperview().offset(-12)
+        }
+    }
+
+}
--- Chef/Chef/LoginViewController.swift
+++ Chef/Chef/LoginViewController.swift
@@ -3,19 +3,22 @@ import SnapKit
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
 
-    var usernameTextField: LoginTextField!
-    var passwordTextField: LoginTextField!
-    var loginSignupButton: UIButton!
-    var switchButton: UIButton!
+    var loginView: LoginView!
     var userIsSigningUp: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.createUI()
+
+        self.loginView = LoginView()
+        self.view.addSubview(self.loginView)
+        self.loginView.snapToSuperview()
+
+        self.loginView.loginSignupButton.addTarget(self, action: #selector(loginSignupButtonTapped), for: .touchUpInside)
+        self.loginView.switchButton.addTarget(self, action: #selector(switchButtonTapped), for: .touchUpInside)
     }
 
     func signUp() {
-        guard let email = self.usernameTextField.text, let password = self.passwordTextField.text else {
+        guard let email = self.loginView.usernameTextField.text, let password = self.loginView.passwordTextField.text else {
             return
         }
         FirebaseManager.signUp(email: email, password: password) { success in
@@ -28,7 +31,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     }
 
     func logIn() {
-        guard let email = self.usernameTextField.text, let password = self.passwordTextField.text else {
+        guard let email = self.loginView.usernameTextField.text, let password = self.loginView.passwordTextField.text else {
             return
         }
         FirebaseManager.login(email: email, password: password) { success in
@@ -49,31 +52,32 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         }
     }
 
+
+    // MARK: - Actions
+
     func switchButtonTapped() {
         self.userIsSigningUp = self.userIsSigningUp ? false : true
 
-        print(self.userIsSigningUp)
-
-        self.loginSignupButton.titleLabel?.alpha = 0.0
-        self.switchButton.titleLabel?.alpha = 0.0
+        self.loginView.loginSignupButton.titleLabel?.alpha = 0.0
+        self.loginView.switchButton.titleLabel?.alpha = 0.0
 
         if self.userIsSigningUp {
-            self.loginSignupButton.setTitle("Sign Up", for: .normal)
-            self.switchButton.setTitle("Login", for: .normal)
+            self.loginView.loginSignupButton.setTitle("Sign Up", for: .normal)
+            self.loginView.switchButton.setTitle("Login", for: .normal)
         } else {
-            self.loginSignupButton.setTitle("Login", for: .normal)
-            self.switchButton.setTitle("Sign Up", for: .normal)
+            self.loginView.loginSignupButton.setTitle("Login", for: .normal)
+            self.loginView.switchButton.setTitle("Sign Up", for: .normal)
         }
 
         UIView.animate(withDuration: 0.6) {
-            self.loginSignupButton.titleLabel?.alpha = 1.0
-            self.switchButton.titleLabel?.alpha = 1.0
+            self.loginView.loginSignupButton.titleLabel?.alpha = 1.0
+            self.loginView.switchButton.titleLabel?.alpha = 1.0
         }
     }
 
     func shakeTextFields() {
-        self.usernameTextField.shake()
-        self.passwordTextField.shake()
+        self.loginView.usernameTextField.shake()
+        self.loginView.passwordTextField.shake()
     }
 
 
@@ -82,113 +86,6 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     func pushToTabBarController() {
         let tabBarController = TabBarController()
         self.navigationController?.pushViewController(tabBarController, animated: true)
-    }
-
-
-    // MARK: - UI
-
-    func createUI() {
-        self.createBackgroundImage()
-        self.createHeaderText()
-        self.createUsernameTextField()
-        self.createPasswordField()
-        self.createLoginSignupButton()
-        self.createSwitchButton()
-    }
-
-    func createBackgroundImage() {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "food-background")
-
-        self.view.addSubview(imageView)
-
-        imageView.snp.makeConstraints { (make) in
-            make.top.left.height.width.equalToSuperview()
-        }
-    }
-
-    func createHeaderText() {
-        let headerLabel = UILabel()
-        headerLabel.text = "Chef"
-        headerLabel.textColor = .white
-        headerLabel.font = UIFont(name: "Avenir-Medium", size: 48)
-
-        self.view.addSubview(headerLabel)
-
-        headerLabel.snp.makeConstraints { (make) in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(75)
-        }
-    }
-
-    func createUsernameTextField() {
-        self.usernameTextField = LoginTextField()
-        self.usernameTextField.keyboardType = .emailAddress
-
-        self.view.addSubview(self.usernameTextField)
-
-        self.usernameTextField.snp.makeConstraints { (make) in
-            make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview().multipliedBy(0.66)
-            make.width.equalToSuperview().multipliedBy(0.8)
-            make.height.equalTo(28)
-        }
-    }
-
-    func createPasswordField() {
-        self.passwordTextField = LoginTextField()
-        self.passwordTextField.isSecureTextEntry = true
-
-        self.view.addSubview(self.passwordTextField)
-
-        self.passwordTextField.snp.makeConstraints { (make) in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(self.usernameTextField.snp.bottom).offset(20)
-            make.width.equalToSuperview().multipliedBy(0.8)
-            make.height.equalTo(28)
-        }
-    }
-
-    func createLoginSignupButton() {
-        self.loginSignupButton = {
-            let lb = UIButton()
-            lb.backgroundColor = .white
-            lb.setTitle("Login", for: .normal)
-            lb.setTitleColor(.black, for: .normal)
-            lb.titleLabel?.font = UIFont(name: "Avenir-Heavy", size: 18)
-            lb.layer.cornerRadius = 5
-            return lb
-        }()
-
-        self.view.addSubview(self.loginSignupButton)
-
-        self.loginSignupButton.snp.makeConstraints { (make) in
-            make.width.equalToSuperview().multipliedBy(0.3)
-            make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview()
-        }
-
-        self.loginSignupButton.addTarget(self, action: #selector(loginSignupButtonTapped), for: .touchUpInside)
-    }
-
-    func createSwitchButton() {
-        self.switchButton = {
-            let sb = UIButton()
-            sb.backgroundColor = .clear
-            sb.setTitle("Sign Up", for: .normal)
-            sb.titleLabel?.textColor = .white
-            sb.titleLabel?.font = UIFont(name: "Avenir-Heavy", size: 16)
-            return sb
-        }()
-
-        self.view.addSubview(self.switchButton)
-
-        self.switchButton.snp.makeConstraints { (make) in
-            make.right.equalToSuperview().offset(-16)
-            make.bottom.equalToSuperview().offset(-12)
-        }
-
-        self.switchButton.addTarget(self, action: #selector(switchButtonTapped), for: .touchUpInside)
     }
     
 }
