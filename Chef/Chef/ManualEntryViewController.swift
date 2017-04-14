b7fac25072f76b68d64ed6789736bf6e02deeaf0
sejdemi <sejan.miah@gmail.com>
2017-04-14 15:02:52 -0400

    Add FridgeViewController

---
+++ Chef/Chef/ManualEntryViewController.swift
@@ -0,0 +1,28 @@
+import UIKit
+import SnapKit
+
+class ManualEntryViewController: UIViewController, ManualEntryViewDelegate{
+    var manualEntryView: ManualEntryView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.manualEntryView = ManualEntryView()
+        self.manualEntryView.delegate = self
+        self.view.addSubview(self.manualEntryView)
+        self.manualEntryView.snapToSuperview()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    func saveFoodButtonTapped() {
+        print(#function)
+    }
+    
+
+
+
+}
--- Chef/Chef/TabBarController.swift
+++ Chef/Chef/TabBarController.swift
@@ -16,11 +16,15 @@ class TabBarController: UITabBarController {
         savedRecipesVC.tabBarItem = UITabBarItem(title: "Saved", image: UIImage(named: "heart"), tag: 2)
         let srNavVC = NavigationController(rootViewController: savedRecipesVC)
 
+        let fridgeVC = FridgeViewController()
+        fridgeVC.tabBarItem = UITabBarItem(title: "Fridge", image: UIImage(named: "heart"), tag: 3)
+        let frNavVC = NavigationController(rootViewController: fridgeVC)
+
         let userSettingsVC = UserSettingsViewController()
         userSettingsVC.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(named: "userIcon"), tag: 4)
         let usNavVC = NavigationController(rootViewController: userSettingsVC)
 
-        let navViewControllers = [gNavVC, srNavVC, usNavVC]
+        let navViewControllers = [gNavVC, srNavVC, frNavVC, usNavVC]
 
         self.setViewControllers(navViewControllers, animated: true)
     }
