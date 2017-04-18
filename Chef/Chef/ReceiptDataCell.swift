3e3a18efdc6dc28d10c8ed8c29f14b306089f59e
sejdemi <sejan.miah@gmail.com>
2017-04-18 17:23:38 -0400

    Refactored DropDownViewController

---
+++ Chef/Chef/ReceiptDataCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReceiptDataCell.swift
+//  Chef
+//
+//  Created by Sejan Miah on 4/18/17.
+//  Copyright © 2017 Blue Team. All rights reserved.
+//
+
+import UIKit
+
+class ReceiptDataCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
