a0045d67135544477ac22b50c57899967370015d
Kayprizzle <kaypree.hodges@gmail.com>
2017-04-18 16:18:49 -0400

    Add button selection persistence

--- Chef/Chef/AddDietView.swift
+++ Chef/Chef/AddDietView.swift
@@ -3,6 +3,25 @@ import UIKit
 
 class AddDietView: UIView {
 
+    var diet: UILabel!
+    var allergies: UILabel!
+    var pescetarian: UIButton!
+    var veg: UIButton!
+    var vegan: UIButton!
+    var paleo: UIButton!
+    var save: UIButton!
+    var dairy: UIButton!
+    var egg: UIButton!
+    var gluten: UIButton!
+    var peanut: UIButton!
+    var sesame: UIButton!
+    var seafood: UIButton!
+    var shellfish: UIButton!
+    var soy: UIButton!
+    var sulfite: UIButton!
+    var treeNut: UIButton!
+    var wheat: UIButton!
+
     var delegate: AddDietDelegate!
 
     override init(frame: CGRect) {
@@ -17,25 +36,28 @@ class AddDietView: UIView {
 
     func commonInit() {
         createBackground()
-        createButtons()
+        createLabels()
+        createDiets()
+        createAllergies()
+        createSaveButton()
     }
 
     func createBackground() {
         self.backgroundColor = .white
     }
 
-    func createButtons() {
-        let diet = UILabel()
+
+    func createLabels() {
+        diet = UILabel()
         diet.text = "Dietary Restrictions"
         diet.font = UIFont(name: "ArialMT", size: 20)
-
         self.addSubview(diet)
         diet.snp.makeConstraints { (make) in
             make.top.equalToSuperview().offset(50)
             make.centerX.equalToSuperview()
         }
 
-        let allergies = UILabel()
+        allergies = UILabel()
         allergies.text = "Allergies"
         allergies.font = UIFont(name: "ArialMT", size: 20)
         self.addSubview(allergies)
@@ -43,34 +65,75 @@ class AddDietView: UIView {
             make.top.equalToSuperview().offset(200)
             make.centerX.equalToSuperview()
         }
+    }
 
-        let save = UIButton()
-        self.addSubview(save)
-        save.backgroundColor = .white
-        save.layer.cornerRadius = 8
-        save.layer.borderColor = Style.flatironBlue.cgColor
-        save.layer.borderWidth = CGFloat(3)
-        save.setTitle(" Save and Close ", for: .normal)
-        save.setTitleColor(Style.flatironBlue, for: .normal)
-        save.titleLabel?.font = UIFont(name: "ArialMT", size: 25)
-        save.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
-        save.snp.makeConstraints { (make) in
-            make.centerX.equalToSuperview()
-            make.bottom.equalToSuperview().offset(-50)
+    func createDiets() {
+        //Dietary Retrictions
+        pescetarian = UIButton()
+        veg = UIButton()
+        vegan = UIButton()
+        paleo = UIButton()
+        self.addSubview(pescetarian)
+        self.addSubview(veg)
+        self.addSubview(vegan)
+        self.addSubview(paleo)
+        pescetarian.addTarget(self, action: #selector(dietButtonTapped), for: .touchUpInside)
+        veg.addTarget(self, action: #selector(dietButtonTapped), for: .touchUpInside)
+        vegan.addTarget(self, action: #selector(dietButtonTapped), for: .touchUpInside)
+        paleo.addTarget(self, action: #selector(dietButtonTapped), for: .touchUpInside)
+
+        pescetarian.backgroundColor = Style.flatironBlue
+        pescetarian.setTitle(" Pescetarian + ", for: .normal)
+        pescetarian.setTitle(" Pescetarian ", for: .selected)
+        pescetarian.layer.cornerRadius = 8
+        pescetarian.snp.makeConstraints { (make) in
+            make.left.equalTo(self.snp.centerX).offset(1.5)
+            make.top.equalTo(diet.snp.bottom).offset(10)
+        }
+        veg.backgroundColor = Style.flatironBlue
+        veg.setTitle(" Vegetarian + ", for: .normal)
+        veg.setTitle(" Vegetarian ", for: .selected)
+        self.addSubview(veg)
+        veg.layer.cornerRadius = 8
+        veg.snp.makeConstraints { (make) in
+            make.right.equalTo(self.snp.centerX).offset(-1.5)
+            make.top.equalTo(diet.snp.bottom).offset(10)
+        }
+        vegan.backgroundColor = Style.flatironBlue
+        vegan.setTitle(" Vegan + ", for: .normal)
+        vegan.setTitle(" Vegan ", for: .selected)
+        self.addSubview(vegan)
+        vegan.layer.cornerRadius = 8
+        vegan.snp.makeConstraints { (make) in
+            make.right.equalTo(self.snp.centerX).offset(-1.5)
+            make.top.equalTo(pescetarian.snp.bottom).offset(3)
         }
 
+        paleo.backgroundColor = Style.flatironBlue
+        paleo.setTitle(" Paleo + ", for: .normal)
+        paleo.setTitle(" Paleo ", for: .selected)
+        self.addSubview(paleo)
+        paleo.layer.cornerRadius = 8
+        paleo.snp.makeConstraints { (make) in
+            make.left.equalTo(self.snp.centerX).offset(1.5)
+            make.top.equalTo(pescetarian.snp.bottom).offset(3)
+        }
+    }
+
+    func createAllergies() {
+
         //Allergies
-        let dairy = UIButton()
-        let egg = UIButton()
-        let gluten = UIButton()
-        let peanut = UIButton()
-        let sesame = UIButton()
-        let seafood = UIButton()
-        let shellfish = UIButton()
-        let soy = UIButton()
-        let sulfite = UIButton()
-        let treeNut = UIButton()
-        let wheat = UIButton()
+        dairy = UIButton()
+        egg = UIButton()
+        gluten = UIButton()
+        peanut = UIButton()
+        sesame = UIButton()
+        seafood = UIButton()
+        shellfish = UIButton()
+        soy = UIButton()
+        sulfite = UIButton()
+        treeNut = UIButton()
+        wheat = UIButton()
         self.addSubview(dairy)
         self.addSubview(egg)
         self.addSubview(gluten)
@@ -94,47 +157,61 @@ class AddDietView: UIView {
         treeNut.addTarget(self, action: #selector(dietButtonTapped), for: .touchUpInside)
         wheat.addTarget(self, action: #selector(dietButtonTapped), for: .touchUpInside)
 
-
-        //Allergies Button setup
-        dairy.backgroundColor = Style.flatironBlue
-        dairy.setTitle(" Dairy + ", for: .normal)
-        dairy.setTitle(" Dairy ", for: .selected)
-        dairy.layer.cornerRadius = 8
-        dairy.snp.makeConstraints { (make) in
-            make.left.equalToSuperview().offset(50)
-            make.top.equalTo(allergies.snp.bottom).offset(10)
-        }
         egg.backgroundColor = Style.flatironBlue
         egg.setTitle(" Egg + ", for: .normal)
         egg.setTitle(" Egg ", for: .selected)
         egg.layer.cornerRadius = 8
         egg.snp.makeConstraints { (make) in
-            make.left.equalTo(dairy.snp.right).offset(3)
+            make.right.equalTo(wheat.snp.left).offset(-1.5)
             make.top.equalTo(allergies.snp.bottom).offset(10)
+        }
+        wheat.backgroundColor = Style.flatironBlue
+        wheat.setTitle(" Wheat + ", for: .normal)
+        wheat.setTitle(" Wheat ", for: .selected)
+        wheat.layer.cornerRadius = 8
+        wheat.snp.makeConstraints { (make) in
+            make.right.equalTo(self.snp.centerX).offset(-1.5)
+            make.top.equalTo(allergies.snp.bottom).offset(10)
+        }
+        soy.backgroundColor = Style.flatironBlue
+        soy.setTitle(" Soy + ", for: .normal)
+        soy.setTitle(" Soy ", for: .selected)
+        soy.layer.cornerRadius = 8
+        soy.snp.makeConstraints { (make) in
+            make.left.equalTo(self.snp.centerX).offset(1.5)
+            make.top.equalTo(allergies.snp.bottom).offset(10)
+        }
+        dairy.backgroundColor = Style.flatironBlue
+        dairy.setTitle(" Dairy + ", for: .normal)
+        dairy.setTitle(" Dairy ", for: .selected)
+        dairy.layer.cornerRadius = 8
+        dairy.snp.makeConstraints { (make) in
+            make.left.equalTo(soy.snp.right).offset(1.5)
+            make.top.equalTo(allergies.snp.bottom).offset(10)
+        }
+
+        peanut.backgroundColor = Style.flatironBlue
+        peanut.setTitle(" Peanut + ", for: .normal)
+        peanut.setTitle(" Peanut ", for: .selected)
+        peanut.layer.cornerRadius = 8
+        peanut.snp.makeConstraints { (make) in
+            make.right.equalTo(sesame.snp.left).offset(-3)
+            make.top.equalTo(dairy.snp.bottom).offset(3)
+        }
+        sesame.backgroundColor = Style.flatironBlue
+        sesame.setTitle(" Sesame + ", for: .normal)
+        sesame.setTitle(" sesame ", for: .selected)
+        sesame.layer.cornerRadius = 8
+        sesame.snp.makeConstraints { (make) in
+            make.right.equalTo(self.snp.centerX).offset(-1.5)
+            make.top.equalTo(dairy.snp.bottom).offset(3)
         }
         gluten.backgroundColor = Style.flatironBlue
         gluten.setTitle(" Gluten + ", for: .normal)
         gluten.setTitle(" Gluten ", for: .selected)
         gluten.layer.cornerRadius = 8
         gluten.snp.makeConstraints { (make) in
-            make.left.equalTo(egg.snp.right).offset(3)
-            make.top.equalTo(allergies.snp.bottom).offset(10)
-        }
-        peanut.backgroundColor = Style.flatironBlue
-        peanut.setTitle(" Peanut + ", for: .normal)
-        peanut.setTitle(" Peanut ", for: .selected)
-        peanut.layer.cornerRadius = 8
-        peanut.snp.makeConstraints { (make) in
-            make.left.equalTo(gluten.snp.right).offset(3)
-            make.top.equalTo(allergies.snp.bottom).offset(10)
-        }
-
-        sesame.backgroundColor = Style.flatironBlue
-        sesame.setTitle(" Sesame + ", for: .normal)
-        sesame.setTitle(" sesame ", for: .selected)
-        sesame.layer.cornerRadius = 8
-        sesame.snp.makeConstraints { (make) in
-            make.left.equalToSuperview().offset(35)
+            make.left.equalTo(self.snp.centerX).offset(1.5)
             make.top.equalTo(dairy.snp.bottom).offset(3)
         }
         seafood.backgroundColor = Style.flatironBlue
@@ -142,23 +219,7 @@ class AddDietView: UIView {
         seafood.setTitle(" Seafood ", for: .selected)
         seafood.layer.cornerRadius = 8
         seafood.snp.makeConstraints { (make) in
-            make.left.equalTo(sesame.snp.right).offset(3)
-            make.top.equalTo(dairy.snp.bottom).offset(3)
-        }
-        soy.backgroundColor = Style.flatironBlue
-        soy.setTitle(" Soy + ", for: .normal)
-        soy.setTitle(" Soy ", for: .selected)
-        soy.layer.cornerRadius = 8
-        soy.snp.makeConstraints { (make) in
-            make.left.equalTo(seafood.snp.right).offset(3)
-            make.top.equalTo(dairy.snp.bottom).offset(3)
-        }
-        wheat.backgroundColor = Style.flatironBlue
-        wheat.setTitle(" Wheat + ", for: .normal)
-        wheat.setTitle(" Wheat ", for: .selected)
-        wheat.layer.cornerRadius = 8
-        wheat.snp.makeConstraints { (make) in
-            make.left.equalTo(soy.snp.right).offset(3)
+            make.left.equalTo(gluten.snp.right).offset(1.5)
             make.top.equalTo(dairy.snp.bottom).offset(3)
         }
 
@@ -167,91 +228,43 @@ class AddDietView: UIView {
         shellfish.setTitle(" Shellfish ", for: .selected)
         shellfish.layer.cornerRadius = 8
         shellfish.snp.makeConstraints { (make) in
-            make.left.equalToSuperview().offset(100)
-            make.top.equalTo(wheat.snp.bottom).offset(3)
+            make.right.equalTo(sulfite.snp.left).offset(-3)
+            make.top.equalTo(seafood.snp.bottom).offset(3)
         }
         sulfite.backgroundColor = Style.flatironBlue
         sulfite.setTitle(" Sulfite + ", for: .normal)
         sulfite.setTitle(" Sulfite ", for: .selected)
         sulfite.layer.cornerRadius = 8
         sulfite.snp.makeConstraints { (make) in
-            make.left.equalTo(shellfish.snp.right).offset(3)
-            make.top.equalTo(wheat.snp.bottom).offset(3)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(seafood.snp.bottom).offset(3)
         }
         treeNut.backgroundColor = Style.flatironBlue
         treeNut.setTitle(" Tree Nut + ", for: .normal)
         treeNut.setTitle(" Tree Nut ", for: .selected)
         treeNut.layer.cornerRadius = 8
         treeNut.snp.makeConstraints { (make) in
-            make.left.equalTo(shellfish.snp.right).offset(3)
-            make.top.equalTo(wheat.snp.bottom).offset(3)
-        }
-
-        //Dietary Retrictions
-        let pescetarian = UIButton()
-        let veg = UIButton()
-        let vegan = UIButton()
-        let lveg = UIButton()
-        let oveg = UIButton()
-        self.addSubview(pescetarian)
-        self.addSubview(veg)
-        self.addSubview(vegan)
-        self.addSubview(lveg)
-        self.addSubview(oveg)
-        pescetarian.addTarget(self, action: #selector(dietButtonTapped), for: .touchUpInside)
-        veg.addTarget(self, action: #selector(dietButtonTapped), for: .touchUpInside)
-        vegan.addTarget(self, action: #selector(dietButtonTapped), for: .touchUpInside)
-        lveg.addTarget(self, action: #selector(dietButtonTapped), for: .touchUpInside)
-        oveg.addTarget(self, action: #selector(dietButtonTapped), for: .touchUpInside)
-
-
-        pescetarian.backgroundColor = Style.flatironBlue
-        pescetarian.setTitle(" Pescetarian + ", for: .normal)
-        pescetarian.setTitle(" Pescetarian ", for: .selected)
-        pescetarian.layer.cornerRadius = 8
-        pescetarian.snp.makeConstraints { (make) in
-            make.left.equalToSuperview().offset(50)
-            make.top.equalTo(diet.snp.bottom).offset(10)
-        }
-        veg.backgroundColor = Style.flatironBlue
-        veg.setTitle(" Vegetarian + ", for: .normal)
-        veg.setTitle(" Vegetarian ", for: .selected)
-        self.addSubview(veg)
-        veg.layer.cornerRadius = 8
-        veg.snp.makeConstraints { (make) in
-            make.left.equalTo(pescetarian.snp.right).offset(3)
-            make.top.equalTo(diet.snp.bottom).offset(10)
-        }
-        vegan.backgroundColor = Style.flatironBlue
-        vegan.setTitle(" Vegan + ", for: .normal)
-        vegan.setTitle(" Vegan ", for: .selected)
-        self.addSubview(vegan)
-        vegan.layer.cornerRadius = 8
-        vegan.snp.makeConstraints { (make) in
-            make.left.equalTo(veg.snp.right).offset(3)
-            make.top.equalTo(pescetarian.snp.top)
-        }
-
-        lveg.backgroundColor = Style.flatironBlue
-        lveg.setTitle(" Lacto Vegetarian + ", for: .normal)
-        lveg.setTitle(" Lacto Vegetarian ", for: .selected)
-        self.addSubview(lveg)
-        lveg.layer.cornerRadius = 8
-        lveg.snp.makeConstraints { (make) in
-            make.left.equalTo(pescetarian.snp.left)
-            make.top.equalTo(pescetarian.snp.bottom).offset(3)
-        }
-
-        oveg.backgroundColor = Style.flatironBlue
-        oveg.setTitle(" Ovo Vegetarian + ", for: .normal)
-        oveg.setTitle(" Ovo Vegetarian ", for: .selected)
-        self.addSubview(oveg)
-        oveg.layer.cornerRadius = 8
-        oveg.snp.makeConstraints { (make) in
-            make.left.equalTo(lveg.snp.right).offset(3)
-            make.top.equalTo(lveg.snp.top)
+            make.left.equalTo(sulfite.snp.right).offset(3)
+            make.top.equalTo(seafood.snp.bottom).offset(3)
         }
         
+    }
+
+    func createSaveButton() {
+        save = UIButton()
+        self.addSubview(save)
+        save.backgroundColor = .white
+        save.layer.cornerRadius = 8
+        save.layer.borderColor = Style.flatironBlue.cgColor
+        save.layer.borderWidth = CGFloat(1)
+        save.setTitle(" Save and Close ", for: .normal)
+        save.setTitleColor(Style.flatironBlue, for: .normal)
+        save.titleLabel?.font = UIFont(name: "ArialMT", size: 20)
+        save.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
+        save.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-50)
+        }
     }
 
     func dietButtonTapped(_ button: UIButton) {
--- Chef/Chef/AddDietViewController.swift
+++ Chef/Chef/AddDietViewController.swift
@@ -3,7 +3,8 @@ import SnapKit
 
 class AddDietViewController: UIViewController, AddDietDelegate {
 
-    var dietaryRestrictions = [String]()
+    var dietList = [String]()
+    var allergyList = [String]()
     let store = RecipeDataStore.shared
     var addDietView: AddDietView!
 
@@ -13,44 +14,90 @@ class AddDietViewController: UIViewController, AddDietDelegate {
         self.addDietView.delegate = self
         self.view.addSubview(addDietView)
         addDietView.snapToSuperview()
-
+        selectButtons()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-        func dietButtonTapped(_ button: UIButton) {
-            var btnTitle = button.title(for: .selected)
+
+    func dietButtonTapped(_ button: UIButton) {
+        var btnTitle = button.title(for: .selected)
+        _ = btnTitle?.characters.popLast()
+        _ = btnTitle?.characters.popFirst()
+        if button.isSelected {
+            button.isSelected = false
+            saveBtnSelection(button, btnTitle: btnTitle)
+            animate(button)
+            if let indexToDelete = self.dietaryRestrictions.index(of: btnTitle!) {
+                self.dietaryRestrictions.remove(at: indexToDelete)
+            }
+        } else {
+            button.isSelected = true
+            saveBtnSelection(button, btnTitle: btnTitle)
+            animate(button)
+            self.dietaryRestrictions.append(btnTitle!)
+        }
+        print(self.dietaryRestrictions)
+    }
+
+    func animate(_ button: UIButton) {
+        UIView.animate(withDuration: 0.1, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: .curveLinear, animations: {
+            button.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        }) { (finished) in
+            UIView.animate(withDuration: 0.1, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: .curveLinear, animations: {
+                button.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            })
+        }
+    }
+
+    func saveButtonTapped() {
+        var user = store.user
+        user.dietList = self.dietList
+        if !dietList.isEmpty{
+            FirebaseManager.addDiet(dietList, to: store.user)
+        }
+        user.allergyList = self.allergyList
+        if !allergyList.isEmpty{
+            FirebaseManager.addAllergy(allergyList, to: store.user)
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    
+    func saveBtnSelection(_ button: UIButton, btnTitle: String?) {
+        let defaults = UserDefaults.standard
+        if button.isSelected{
+            if let btnTitle = btnTitle {
+                defaults.set(true, forKey: btnTitle)
+            }
+        } else {
+            if let btnTitle = btnTitle {
+                defaults.set(false, forKey: btnTitle)
+            }
+        }
+
+    }
+
+    func selectButtons() {
+        let allFoodButtons = [addDietView.pescetarian, addDietView.veg, addDietView.vegan, addDietView.paleo, addDietView.dairy, addDietView.egg, addDietView.gluten, addDietView.peanut, addDietView.sesame, addDietView.seafood, addDietView.shellfish, addDietView.soy, addDietView.sulfite, addDietView.treeNut, addDietView.wheat]
+        let defaults = UserDefaults.standard
+
+        for button in allFoodButtons {
+            var btnSelected = false
+            var btnTitle = button?.title(for: .selected)
             _ = btnTitle?.characters.popLast()
             _ = btnTitle?.characters.popFirst()
-            if button.isSelected {
-                button.isSelected = false
-                animate(button)
-                if let indexToDelete = self.dietaryRestrictions.index(of: btnTitle!) {
-                    self.dietaryRestrictions.remove(at: indexToDelete)
-                }
+            if let btnTitle = btnTitle {
+                btnSelected = defaults.bool(forKey: btnTitle)
             } else {
-                button.isSelected = true
-                animate(button)
-                self.dietaryRestrictions.append(btnTitle!)
+                btnSelected = false
             }
-           print(self.dietaryRestrictions)
-        }
-        
-        func animate(_ button: UIButton) {
-            UIView.animate(withDuration: 0.1, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: .curveLinear, animations: {
-                button.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
-            }) { (finished) in
-                UIView.animate(withDuration: 0.1, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: .curveLinear, animations: {
-                    button.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
-                })
+
+            if btnSelected {
+                button?.isSelected = true
             }
         }
-        
-        func saveButtonTapped() {
-            var user = store.user
-            user.dietaryRestrictions = self.dietaryRestrictions
-            self.dismiss(animated: true, completion: nil)
-        }
+    }
 
 }
