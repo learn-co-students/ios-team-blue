e2bcb89777a4ab687b62afbabcca0c496a0c64ac
sejdemi <sejan.miah@gmail.com>
2017-04-18 14:53:02 -0400

    Add TableView to ScanReceiptViewController

--- Chef/Chef/ScannedReceiptViewController.swift
+++ Chef/Chef/ScannedReceiptViewController.swift
@@ -1,11 +1,12 @@
 import UIKit
 import SnapKit
 
-class ScannedReceiptViewController: UIViewController {
+class ScannedReceiptViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
     var urlToLastPhoto: URL! = {
         return DropDownViewController.getDocumentsDirectory()
     }()
+    var tableView: UITableView!
 
 
 
@@ -17,7 +18,36 @@ class ScannedReceiptViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    
+
+    // MARK: - Data Source
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+
+    // MARK: - Delegate
+
+    // MARK: - UI
+
+    func createUI() {
+
+        self.tableView = {
+            let tv = UITableView(frame: self.view.frame)
+            tv.dataSource = self
+            tv.delegate = self
+            tv.layer.borderColor = Style.flatironBlue.cgColor
+            return cv
+        }()
+
+        self.view.addSubview(self.tableView)
+    }
+
+
+
+
     
 }
