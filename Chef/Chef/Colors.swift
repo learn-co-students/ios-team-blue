9706b5b5c0b4ecdebac64542221cdaa9703281de
Carlos Ivan Hernandez <carlos.i.hernandez011@gmail.com>
2017-04-19 01:59:07 -0400

    Separate Style namespace into Colors and Fonts namespaces

--- Chef/Chef/Colors.swift
+++ Chef/Chef/Colors.swift
@@ -1,9 +1,8 @@
 import UIKit
 
-struct Style {
+struct Colors {
 
     static let flatironBlue = UIColor(red: 0, green: 0.698, blue: 0.906, alpha: 1.0)
-    static let regular = "Avenir-Medium"
-    static let bold = "Avenir-Heavy"
+    static let veryLightGray = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1.0)
 
 }
---
+++ Chef/Chef/Fonts.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+struct Fonts {
+
+    static let regular = UIFont(name: "Avenir-Medium", size: 14)!
+    static let heavy = UIFont(name: "Avenir-Heavy", size: 14)!
+
+}
