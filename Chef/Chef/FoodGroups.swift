4a731308ad04285ab123f490499b8369acd275e2
Kayprizzle <kaypree.hodges@gmail.com>
2017-04-20 10:45:38 -0400

    Change Ingredient class to FoodGroups

---
+++ Chef/Chef/FoodGroups.swift
@@ -0,0 +1,14 @@
+//
+//  FoodGroups.swift
+//  Chef
+//
+//  Created by Kaypree Hodges on 4/19/17.
+//  Copyright © 2017 Blue Team. All rights reserved.
+//
+
+import Foundation
+
+struct FoodGroups {
+    var groupName: String
+    var groupItems: [String]
+}
