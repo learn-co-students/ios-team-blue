04d7fed738007b20b01e2da6e839df6acb25cf98
Carlos Ivan Hernandez <carlos.i.hernandez011@gmail.com>
2017-04-20 13:54:38 -0400

    Add print statements and add removeSavedRecipe function

--- Chef/Chef/RecipeDataStore.swift
+++ Chef/Chef/RecipeDataStore.swift
@@ -15,11 +15,12 @@ final class RecipeDataStore {
     }
 
     func getRecipes(_ completion: @escaping () -> ()) {
-        print("We are getting recipes!")
-        SpoonacularAPIClient.generateRecipes(for: self.user) { (result) in
+        print("RecipeDataStore -- \(#function)")
+
+        SpoonacularAPIClient.generateRecipes(for: self.user) { result in
             switch result {
             case .success(let recipeList):
-                print("We succesfully got the recipes!")
+                print("RecipeDataStore -- \(#function) -- Success")
                 guard let recipeList = recipeList as? [[String: Any]] else {
                     return
                 }
@@ -28,34 +29,63 @@ final class RecipeDataStore {
                     if let recipe = Recipe(dictionary: dictionary) {
                         self.recipes.append(recipe)
                     } else {
-                        print("recipe skipped")
+                        print("RecipeDataStore -- \(#function) -- Recipe was filtered out")
                     }
                 }
                 completion()
             case .failure(let error):
-                print("We did not get the recipes :(")
-                print("The error is ", error.localizedDescription)
+                print("RecipeDataStore -- \(#function) -- Error: \(error)")
             }
         }
     }
 
     func updateSavedRecipes(completion: @escaping () -> ()) {
+        print("RecipeDataStore -- \(#function)")
+
         SpoonacularAPIClient.fetchSavedRecipes(for: self.user) { result in
             switch result {
             case .success(let recipes):
                 guard let recipes = recipes as? [Recipe] else {
-                    print(#function + " casting failed")
+                    print("RecipeDataStore -- \(#function) -- Casting failed")
                     return
                 }
                 self.savedRecipes = recipes
+                print("RecipeDataStore -- \(#function) -- updatingSavedRecipes")
                 completion()
             case .failure(let error):
-                print(error)
+                print("RecipeDataStore -- \(#function) -- Error: \(error)")
             }
         }
     }
 
+    func removeSavedRecipe(_ recipe: Recipe, completion: @escaping () -> ()) {
+        print("RecipeDataStore -- \(#function)")
+
+        var newSavedRecipes = [Recipe]()
+
+        // make new array for saved recipes that doesn't include the remove recipe
+        for savedRecipe in self.savedRecipes {
+            if recipe.id != savedRecipe.id {
+                newSavedRecipes.append(savedRecipe)
+            }
+        }
+
+        // make dictionary from newSavedRecipes array
+        var savedRecipeDict = JSONDictionary()
+        for (index, recipe) in newSavedRecipes.enumerated() {
+            savedRecipeDict["\(index)"] = recipe.id
+        }
+
+        //FirebaseManager.setSavedRecipes(savedRecipeDict, for: self.user)
+
+        self.updateSavedRecipes {
+            completion()
+        }
+    }
+
     func pullDataForUser(_ user: User, completion: @escaping () ->()) {
+        print("RecipeDataStore -- \(#function)")
+
         FirebaseManager.getUserData(user) { (recipeIDs, food, diet, allergies) in
             for id in recipeIDs {
                 self.user.favRecipes.append(id)
